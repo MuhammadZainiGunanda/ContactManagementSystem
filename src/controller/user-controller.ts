a90653aaee711b582dd21f4c046ef09a02a10bfd
muhzainn_ <137037405+zennsketchy@users.noreply.github.com>
2024-04-27 13:08:33 +0700

    User management (#5)
    * Contact-Management: Menyempurnakan Komentar pada Operasi Terkait:
    
    Memodifikasi beberapa file untuk menambahkan komentar yang menjelaskan struktur operasi terkait.
    Komentar tersebut bertujuan untuk memberikan kejelasan tambahan mengenai fungsi dan tujuan dari setiap operasi.
    Semua perubahan telah direview dan diuji sebelum dimasukkan ke dalam branch utama.
    
    * User-Management: fix duplikat code in file 'private-api'

--- src/controller/user-controller.ts
+++ src/controller/user-controller.ts
@@ -9,56 +9,67 @@ export class UserController {
 
      static async createUserRegistration(request : Request, response : Response, next : NextFunction) : Promise<void> {
           try {
+               // Manggil layanan untuk proses buat user baru, dan nunggu konfirmasi
                const registrationConfirmation : UserOperationOutcome | ResponseError = 
                     await UserService.submitUserRegistration(request.body);
 
+               // Jika OK, respons 200 serta data dari hasil konfirmasi
                response.status(200).json({ data: registrationConfirmation });
           } catch (error) {
-               next(error);
+               next(error); // Lempar Error ke middleware selanjutnya
           }
      }
 
      static async loginUserCredentials(request : Request, response : Response, next : NextFunction) : Promise<void> {
           try {
+               // Manggil layanan untuk prsess user login, dan nunggu konfirmasi
                const loginUserConfirmation : UserOperationOutcome | ResponseError = 
                     await UserService.submitUserLogin(request.body);
 
-               const createToken = jwt.sign({ ...loginUserConfirmation }, process.env.TOKEN_SECRET_KEY!, { expiresIn: process.env.EXPIRES_IN! });
-
+               // Jika OK, buat token JWT dengan data konfirmasi login, dengan mengatur waktu kedaluwarsa
+               const createToken = jwt.sign({ ...loginUserConfirmation }, 
+                    process.env.TOKEN_SECRET_KEY!, { expiresIn: process.env.EXPIRES_IN! });
+               
+               // Atur cookie dengan token JWT untuk otentikasi sesi
                response.cookie("login", createToken, { httpOnly: true, secure: true, sameSite: "strict" })
                .status(200).json({ data: loginUserConfirmation });
           } catch (error) {
-               next(error);
+               next(error); // Lempar Error ke middleware selanjutnya
           }
      }
 
      static async getUser(request : RequestUserValidator, response : Response, next : NextFunction) : Promise<void> {
           try {
-               const getUserConfirmation : UserOperationOutcome = await UserService.submitGetUser(request.user!);
+               // Manggil layanan untuk proses mendapatkan data user, dan nunggu konfirmasi
+               const getUserConfirmation : UserOperationOutcome | ResponseError = 
+                    await UserService.submitGetUser(request.user!);
 
+               // Jika OK, respons 200, serta data dari hasil konfirmasi
                response.status(200).json({ data: getUserConfirmation });
           } catch (error) {
-               next(error);
+               next(error); // Lempar Error ke middleware selanjutnya
           }
      }
 
      static async updateUser(request : RequestUserValidator, response : Response, next : NextFunction) : Promise<void>  {
           try {
-               const updateUserConfirmation = await UserService.submitUpdateUser(request.user!, request.body);
+               // Manggil layanan untuk proses update record user, dan nunggu konfirmasi
+               const updateUserConfirmation : UserOperationOutcome | ResponseError = 
+                    await UserService.submitUpdateUser(request.user!, request.body);
 
-               console.info(updateUserConfirmation);
-               
+               // Jika OK, respons 200, serta data dari hasil konfirmasi
                response.status(200).json({ data: updateUserConfirmation });
           } catch (error) {
-               next(error);
+               next(error); // Lempar Error ke middleware selanjutnya
           }
      }
 
      static async logoutUser(request : RequestUserValidator, response : Response, next : NextFunction) : Promise<void> {
           try {
+               // Clear cookie, serta respons 200 sebagai tanda proses logout berhasil
                response.clearCookie("login").status(200).json({ data: "OK" });
           } catch (error) {
-               next(error);
+               next(error); // Lempar Error ke middleware selanjutnya
           }
      }
 
--- src/model/user-management.ts
+++ src/model/user-management.ts
@@ -1,27 +1,32 @@
 import { User } from "@prisma/client";
 
+// Tipe data untuk input proses registrasi
 export type InputUserRegistration = {
      username : string;
      name : string;
      password : string;
 }
 
+// Tipe data untuk input proses login user
 export type InputUserLogin = {
      username : string;
      password : string;
 }
 
+// Tipe data untuk input proses update user
 export type InputUserUpdate = {
      name : string;
      password : string;
 }
 
+// Tipe data untuk proses terkait
 export type UserOperationOutcome = {
      username : string;
      name : string;
      token? : string;
 }
 
+// Method untuk konveri payload user dari Prisma
 export function convertToUserResponseOutcome(userPayload : User) : UserOperationOutcome {
      return { username: userPayload.username, name: userPayload.name };
 }
